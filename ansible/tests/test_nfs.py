9c8d335b06274ce980b21891b90ad572e32144d7
andygma567 <andygma567@gmail.com>
2025-10-12 16:04:31 -0500

    fixup! fixup! fix: generate the inventory.yml in correct place

--- ansible/tests/test_nfs.py
+++ ansible/tests/test_nfs.py
@@ -5,10 +5,12 @@ inventory = AnsibleRunner("build/inventory.ini")
 servers = inventory.get_hosts("nfs_servers")
 clients = inventory.get_hosts("nfs_clients")
 
+
 @pytest.mark.parametrize("host", servers)
 def test_nfs_server_service_running(host):
     names = ["nfs-server", "nfs-kernel-server"]
     assert any(host.service(n).is_running for n in names)
+
 
 @pytest.mark.parametrize("host", clients)
 def test_nfs_client_mount_point_exists(host):
